56992ad3b0ef4a5948566655f395bbd6558531db
Varun <kumar12345678varun@gmail.com>
2025-07-19 23:40:03 +0530

    Student

--- StudentMarsheetProject/StudentMarkSheet.java
+++ StudentMarsheetProject/StudentMarkSheet.java
@@ -2,31 +2,36 @@
 import java.util.Scanner;
 
 public class StudentMarkSheet {
+
     public static void main(String[] args) {
         InputDetails input = new InputDetails();
         input.inputDetails();
     }
 }
 
-class InputDetails{
-    void inputDetails(){
+
+
+
+class InputDetails {
+
+    void inputDetails() {
         Scanner inp = new Scanner(System.in);
         String[] subjects = {"Telugu", "Hindi", "English", "Maths", "Physics", "Chemistry", "Biology", "Social Studies"};
         int subLen = subjects.length;
         char choice;
 
-        do { 
+        do {
             System.out.println("Subjects : ");
-            for(int i = 0 ; i < subLen ; i++){
-                System.out.print(i+1 + ". " + subjects[i] + ".\n");
+            for (int i = 0; i < subLen; i++) {
+                System.out.print(i + 1 + ". " + subjects[i] + ".\n");
             }
             System.out.println("Enter Marks with Space.");
-            for(String s : subjects){
-                System.out.print(s+ " ");
+            for (String s : subjects) {
+                System.out.print(s + " ");
             }
             String[] marksSt = inp.nextLine().split(" ");
-            for(String )
-            System.out.print("\n\nDo you want continue? [y/n] : ");
+            for (String )
+                System.out.print("\n\nDo you want continue? [y/n] : ");
             choice = inp.next().charAt(0);
         } while (choice == 'y' || choice == 'Y');
     }
